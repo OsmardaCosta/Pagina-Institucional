55cae040cc58bdad0d77168a19b13e1a78044033
Osmar Costa <151251793+OsmardaCosta@users.noreply.github.com>
2024-06-05 20:13:25 +0100

    Update InputText.jsx

--- src/Components/InputText.jsx
+++ src/Components/InputText.jsx
@@ -1,6 +1,6 @@
 const InputText = ({placeholder, id, type}) =>{
     return(
-        <input className="focus:border-blue-600 transition border border-zinc-400 rounded p-1 mb-1 outline-none w-[99%]" type={type} id={id} placeholder={placeholder} required/>
+        <input className="focus:border-blue-600 transition border border-zinc-400 rounded p-2 mb-1 outline-none w-[99%] md:p-1" type={type} id={id} placeholder={placeholder} required/>
     )
 }
 
