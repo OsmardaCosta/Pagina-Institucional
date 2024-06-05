b05667c7e2d6d82d49b944192bd9211478e6db62
Osmar Costa <151251793+OsmardaCosta@users.noreply.github.com>
2024-06-05 20:28:14 +0100

    set

--- src/Components/Select.jsx
+++ src/Components/Select.jsx
@@ -3,8 +3,8 @@ const Select = ({id, options}) =>{
     return(
         <div>
             <select id={id} className="outline-none w-[99%] border border-zinc-600 p-1 rounded cursor-pointer transition focus:border-blue-600">
-                {options.map((option, index) => (
-                    <option value={option.value} key={index}>
+                {options.map((option, index, array) => (
+                    <option value={option.value} key={index} selected={array[7]}>
                         {option.value}
                     </option>
                 ))}
